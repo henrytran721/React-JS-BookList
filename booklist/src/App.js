432f8a6dc6d6f231fb1b54ebd88e8aa50a92a4b4
henrytran721 <htran@ucsd.edu>
2019-05-24 19:23:16 -0700

    add and delete book functions

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -4,36 +4,58 @@ import './App.css';
 
 function AddBook(props) {
   return (
-    <form className="bookInfo">
+    <div className="bookInfo">
     <div className="bookHeader"><p>Add New Book</p></div>
       <div className="bookNameInput">
         <p>Book Title</p>
         <input type="text"
-        value ={props.titleValue}
-        onChange={props.onChange}
+        value={props.titleValue}
+        onChange={props.onTitleChange}
         required/>
       </div>
       <div className="bookNameInput">
         <p>Author</p>
-        <input type="text" required/>
+        <input type="text"
+        value={props.authorValue}
+        onChange={props.onAuthorChange}
+        required/>
       </div>
       <div className="bookNameInput">
         <p>Number of Pages</p>
-        <input type="number" required/>
+        <input
+        type='number'
+        value={props.pageValue}
+        onChange={props.onPageChange}
+        />
       </div>
       <div className="btns">
-        <button>Submit</button>
+        <button onClick={() => props.onCreateNewBook()}>Submit</button>
         <button onClick={() => props.onCancelForm()}>Cancel</button>
       </div>
-    </form>
+    </div>
   )
 }
 
 function ReadingList(props) {
-  if(props.books.length === 0) {
+  if(props.list.length === 0) {
     return (
       <div className="noBook">
        <p>You currently have no books. Please click 'Add Book' to get started!</p>
+      </div>
+    )
+  } else {
+    return (
+        <div className='bookContainer'>
+          {props.list.map((books) => (
+            <div key={books.bookName} className='bookListDiv'>
+              <button onClick={() => props.onRemoveBook(books.bookName)} className='deleteBtn'><span>X</span></button>
+              <div key={books.bookName}>
+                <span className='bookName'><strong><p>Book Title:&nbsp;</p></strong>{books.bookName}</span>
+                <span className='authorName'><strong><p>Author:&nbsp;</p></strong>{books.authorName}</span>
+                <span className='pageNum'><strong><p>Number of Pages:&nbsp;</p></strong>{books.pageNum}</span>
+              </div>
+          </div>
+          ))}
       </div>
     )
   }
@@ -46,16 +68,23 @@ class App extends Component {
       addForm: false,
       books: [],
       bookTitleInput: '',
-      authorInput: ''
+      authorInput: '',
+      pagesInput: ''
     }
     this.showForm = this.showForm.bind(this)
-    this.updateInput = this.updateInput.bind(this)
+    this.updateTitleInput=this.updateTitleInput.bind(this)
+    this.updateAuthorInput=this.updateAuthorInput.bind(this)
+    this.updatePagesInput=this.updatePagesInput.bind(this)
+    this.removeBook=this.removeBook.bind(this)
+    this.createNewBook=this.createNewBook.bind(this)
     this.cancelForm = this.cancelForm.bind(this)
   }
 
   cancelForm() {
     let mainBody = document.querySelector('.background');
+    let addBookForm = document.querySelector('.addBookForm');
     mainBody.style.filter = "blur(0px)";
+    addBookForm.style.transform =  '';
     this.setState({
       addForm: false
     })
@@ -63,10 +92,13 @@ class App extends Component {
 
   showForm() {
     let mainBody = document.querySelector('.background');
+    let addBookForm = document.querySelector('.addBookForm');
     if(this.state.addForm !== true) {
       mainBody.style.filter = "blur(8px)";
+      addBookForm.style.transform = 'translateY(80px)'
     } else {
       mainBody.style.filter = "blur(0px)";
+      addBookForm.style.transform = 'translateY(-80px)'
     }
     this.setState((currentState) => {
       return {
@@ -74,12 +106,58 @@ class App extends Component {
       }
     })
   }
-
-  updateInput(e) {
+  updateTitleInput(e) {
     const bookTitleValue=e.target.value;
-    console.log(this.state.bookTitleInput);
+    console.log("Title Input: " + this.state.bookTitleInput);
     this.setState({
       bookTitleInput: bookTitleValue
+    })
+  }
+  updateAuthorInput(e) {
+    const authorValue=e.target.value;
+    console.log("Author Input: " + this.state.authorInput);
+    this.setState({
+      authorInput: authorValue
+    })
+  }
+  updatePagesInput(e) {
+    console.log('Pages: ' + e.target.value)
+    if(e.target.validity.valid) {
+      this.setState({
+        pagesInput: e.target.value
+      })
+    }
+  }
+  createNewBook() {
+    let mainBody = document.querySelector('.background');
+    if(this.state.bookTitleInput.length !== 0) {
+      mainBody.style.filter = 'blur(0px)'
+    this.setState((currentState) => {
+      return {
+        addForm: !currentState.addForm,
+        books: currentState.books.concat([
+          {
+            bookName: this.state.bookTitleInput.charAt(0).toUpperCase()
+            + this.state.bookTitleInput.slice(1),
+            authorName: this.state.authorInput,
+            pageNum: this.state.pagesInput
+          }
+        ]),
+        bookTitleInput: '',
+        authorInput: '',
+        pagesInput: ''
+      }
+    })
+    console.log(this.state.books);
+  }
+  }
+
+  removeBook(name) {
+    console.log(this.state.books);
+    this.setState((currentState) => {
+      return {
+        books: currentState.books.filter((book) => book.bookName !== name)
+      }
     })
   }
 
@@ -94,15 +172,21 @@ class App extends Component {
               <button onClick={() => this.showForm()} className="addButton"><span
               className="addSymbol">+</span>Add Book</button>
               <ReadingList
-                books={this.state.books}
+                list={this.state.books}
+                onRemoveBook={this.removeBook}
               />
           </main>
         </div>
         <div className="addBookForm">
         {this.state.addForm ? <AddBook
           titleValue={this.state.bookTitleInput}
-          onChange={this.updateInput}
-          onCancelForm = {this.cancelForm}
+          onTitleChange={this.updateTitleInput}
+          authorValue={this.state.authorInput}
+          onAuthorChange={this.updateAuthorInput}
+          pageValue={this.state.pagesInput}
+          onPageChange={this.updatePagesInput}
+          onCancelForm={this.cancelForm}
+          onCreateNewBook={this.createNewBook}
         /> : null }
         </div>
       </div>
